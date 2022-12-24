015071f1715984f65e7b162b4b13f98b2547cff6
gunel <lothlovien@gmail.com>
2022-12-24 17:40:32 +0400

    первый пример второй задачи

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 2 Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+{
+    Console.WriteLine("max=b");
+}
+else{
+    Console.WriteLine("max=a");
+}
